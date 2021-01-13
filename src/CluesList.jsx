a4c7a50a7b54c3a058997c1513369eef50117629
Delong Fu <delongfu@gmail.com>
2021-01-12 16:55:54 -0800

    scrolling clue

--- src/CluesList.jsx
+++ src/CluesList.jsx
@@ -1,32 +1,81 @@
-import React, { useCallback, useContext } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+  createRef,
+} from "react";
 import { CluesContext } from "./App";
 
-const CluesList = ({ game, direction, highlighted }) => {
+function clueKey(cn, direction) {
+  return `clue-${direction}-${cn}`;
+}
+
+const CluesList = ({ game, direction }) => {
+  const [currCn, setCurrCn] = useState(0);
+
   const cluesMap = direction === 0 ? game.cluesAcross : game.cluesDown;
   const clues = Object.keys(cluesMap).map((id) => cluesMap[id]);
   const { currentClueIndices } = useContext(CluesContext);
 
+  const refs = clues.reduce((acc, clue) => {
+    acc[clueKey(clue.cn, direction)] = createRef();
+    return acc;
+  }, {});
+
+  const scrollToClue = useCallback(
+    (id) => {
+      if (!refs[id]) {
+        return;
+      }
+
+      console.log("scrolling to ", id);
+
+      refs[id].current.scrollIntoView({
+        behavior: "auto",
+        block: "center",
+      });
+    },
+    [refs]
+  );
+
+  useEffect(() => {
+    let retval = "";
+    if (direction === 0) {
+      retval = currentClueIndices.across;
+    } else {
+      retval = currentClueIndices.down;
+    }
+    setCurrCn(retval);
+    const clueToScrollTo = clueKey(retval, direction);
+    scrollToClue(clueToScrollTo);
+  }, [currentClueIndices, direction, scrollToClue]);
+
   const className = useCallback(
     (clue) => {
-      if (direction === 0 && clue.cn === currentClueIndices.across) {
-        return "selected";
-      } else if (direction === 1 && clue.cn === currentClueIndices.down) {
+      if (clue.cn === currCn) {
         return "selected";
       }
       return null;
     },
-    [currentClueIndices, direction]
+    [currCn]
   );
 
   return (
-    <div className="clue-list" key={`clues-list-${direction}`}>
+    <div>
       <h2>{direction === 0 ? "Across" : "Down"}</h2>
-      {clues.map((clue) => (
-        <div key={`clue-${direction}-${clue.cn}`} className={className(clue)}>
-          <span>{clue.cn}.&nbsp;</span>
-          {decodeURIComponent(clue.c)}
-        </div>
-      ))}
+      <div className="clue-list" key={`clues-list-${direction}`}>
+        {clues.map((clue) => (
+          <div
+            key={clueKey(clue.cn, direction)}
+            ref={refs[clueKey(clue.cn, direction)]}
+            className={className(clue)}
+          >
+            <span>{clue.cn}.&nbsp;</span>
+            {decodeURIComponent(clue.c)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
